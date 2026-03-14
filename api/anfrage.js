2dd7af005d9facf9bf0d1eeef7f22e94814d2410
Kai Weinschenck <info@weinschenck-garten.de>
2026-03-14 01:34:27 +0100

    move api route to root

--- api/anfrage.js
+++ api/anfrage.js
@@ -1,13 +1,18 @@
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
 
   try {
-    const data = req.body;
 
-    console.log("Neue Gartenanfrage:", data);
+    console.log("Neue Gartenanfrage eingegangen");
 
     return res.status(200).json({
       success: true,
@@ -15,9 +20,12 @@ export default async function handler(req, res) {
     });
 
   } catch (error) {
+
     return res.status(500).json({
       success: false,
       error: "Serverfehler"
     });
+
   }
+
 }
